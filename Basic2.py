57b3eeadafc11d66aa7e20e9f96f039cd9fd4651
rajashree0705 <r.borah2020@gmail.com>
2025-05-24 12:16:28 +0530

    Add files via upload

---
+++ Basic2.py
@@ -0,0 +1,4 @@
+num1 = int(input("Enter first integer:"))
+num2 = int(input("Enter secont integer:"))
+sum = num1+num2
+print("The sum is:", sum)
---
+++ Basic3.py
@@ -0,0 +1,6 @@
+divident = int(input("Enter the divident:"))
+divisor = int(input("Enter the divisor"))
+quotient = divident//divisor
+remainder = divident % divisor
+print("Quotient is :", quotient)
+print("Remainder is:", remainder)
