d526f9729ed31d5ac2db1d2f9e1f6c6573d7c580
rajashree0705 <r.borah2020@gmail.com>
2025-05-24 12:39:10 +0530

    Delete Basic3.py

--- Basic3.py
+++
@@ -1,6 +0,0 @@
-divident = int(input("Enter the divident:"))
-divisor = int(input("Enter the divisor"))
-quotient = divident//divisor
-remainder = divident % divisor
-print("Quotient is :", quotient)
-print("Remainder is:", remainder)
